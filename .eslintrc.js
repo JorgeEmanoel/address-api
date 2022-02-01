d4a66edc4e3a26fecb250880585ed22a09797353
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-01 16:42:53 -0300

    config: lint and ts

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  env: {
+    es2021: true,
+    node: true
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'standard'
+  ],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module'
+  },
+  plugins: [
+    '@typescript-eslint'
+  ],
+  rules: {
+    indent: ['error', 2]
+  }
+}
---
+++ README.md
@@ -0,0 +1,14 @@
+## Address Manager
+
+Este projeto consiste em uma API para gerenciamento de endereços por usuário.
+
+### Tecnologias
+
+- [nodejs](https://nodejs.org/en/)
+- [typescript](https://www.typescriptlang.org/)
+- [mysql](https://www.mysql.com/)
+- [typeorm](https://typeorm.io/)
+
+### Instalação
+
+Seção em construção.
