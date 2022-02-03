8bb7b92324700c7706b324d5b1c3feaf283dcd91
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-03 20:41:06 -0300

    feat(rules): create availableRules constants

---
+++ src/constants/validator/availableRules.ts
@@ -0,0 +1,15 @@
+const RULE_MIN = 'min'
+const RULE_MAX = 'max'
+const RULE_REQUIRED = 'required'
+
+export {
+  RULE_MIN,
+  RULE_MAX,
+  RULE_REQUIRED
+}
+
+export default [
+  RULE_MIN,
+  RULE_MAX,
+  RULE_REQUIRED
+]
