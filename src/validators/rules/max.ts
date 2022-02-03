58975b3bc23b92b5b7d803e718aaa4b6932ef955
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-03 20:40:54 -0300

    feat(rules): create max rule

---
+++ src/validators/rules/max.ts
@@ -0,0 +1,28 @@
+import IRulable from '../../contracts/iRulable'
+
+class MaxRule implements IRulable<string | number> {
+  value: string | number
+
+  constructor () {
+    this.value = Number.POSITIVE_INFINITY
+  }
+
+  withValue (value: string | number) {
+    this.value = value
+    return this
+  }
+
+  isValid (data: string | number) {
+    if (typeof data === 'string') {
+      return data.length <= this.value
+    }
+
+    return Number(data) <= this.value
+  }
+
+  message (fieldName: string) {
+    return `The field ${fieldName} must have up to ${this.value} chars`
+  }
+}
+
+export default MaxRule
