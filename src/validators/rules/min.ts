fc6d86ae4563cb113c0cfc5673998c5c6898b14c
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-03 20:40:46 -0300

    feat(rules): create min rule

---
+++ src/validators/rules/min.ts
@@ -0,0 +1,28 @@
+import IRulable from '../../contracts/iRulable'
+
+class MinRule implements IRulable<string | number> {
+  value: string | number
+
+  constructor () {
+    this.value = Number.NEGATIVE_INFINITY
+  }
+
+  withValue (value: string | number) {
+    this.value = value
+    return this
+  }
+
+  isValid (data: string | number) {
+    if (typeof data === 'string') {
+      return data.length >= this.value
+    }
+
+    return Number(data) >= this.value
+  }
+
+  message (fieldName: string) {
+    return `The field ${fieldName} must have at least ${this.value} chars`
+  }
+}
+
+export default MinRule
