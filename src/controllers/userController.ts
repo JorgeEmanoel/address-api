17f3491f499523afa32982221ab8480c7939aa4c
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-03 20:44:56 -0300

    feat(user CRUD): store, auth and update controller methods

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,7 +1,12 @@
 import { Request, Response } from 'express'
-import response from '../constants/http/response'
-import IController from '../contracts/iController'
-import repo from '../repositories/userRepository'
+import Validator from '../validators/validator'
+import ResponseConstants from '../constants/http/response'
+import RecordedUserDTO from '../dto/recordedUserDTO'
+import UserRepository, { CountProps, StoreProps, UpdateProps, FindProps, FindResult } from '../repositories/userRepository'
+import IRepository from '../contracts/iRepository'
+import Hasher from '../security/hasher'
+import UserDTO from '../dto/userDto'
+import IHash from '../contracts/iHash'
 
 interface StoreBodyProps extends Request {
   body: {
@@ -11,29 +16,160 @@ interface StoreBodyProps extends Request {
   }
 }
 
-class UserController implements IController {
-  public index (req: Request, res: Response): Response {
+interface LoginBodyProps extends Request {
+  body: {
+    email: string
+    password: string
+  }
+}
+
+interface AuthenticatedRequest extends Request {
+  user: RecordedUserDTO
+}
+
+class UserController {
+  private _validator: Validator
+  private _hasher: IHash
+  private _repo: IRepository<
+    UserDTO,
+    CountProps,
+    StoreProps,
+    UpdateProps,
+    FindProps,
+    FindResult
+  >
+
+  constructor (validator: Validator, hasher: IHash | null = null) {
+    this._validator = validator
+    if (!hasher) {
+      hasher = new Hasher()
+    }
+    this._hasher = hasher
+    this._repo = new UserRepository(hasher)
+  }
+
+  show (req: Request, res: Response): Response {
     return res.send()
   }
 
-  public async store (req: StoreBodyProps, res: Response) {
+  async auth (req: LoginBodyProps, res: Response) {
+    const fails = this._validator.withData(req.body).withRules([
+      {
+        fieldName: 'email',
+        rules: ['required']
+      },
+      {
+        fieldName: 'password',
+        rules: ['required']
+      }
+    ]).fails()
+
+    if (fails) {
+      return res.status(ResponseConstants.HTTP_BAD_REQUEST).send({
+        message: 'Invalid data',
+        errors: this._validator.errors()
+      })
+    }
+
+    const { email, password } = req.body
+
+    const data = await this._repo.find({ email })
+
+    if (!data) {
+      return res.status(ResponseConstants.HTTP_FORBIDDEN).send({
+        message: 'Invalid e-mail or password'
+      })
+    }
+
+    if (data.password !== this._hasher.make(password)) {
+      return res.status(ResponseConstants.HTTP_FORBIDDEN).send({
+        message: 'Invalid e-mail or password'
+      })
+    }
+
+    // TODO: login user
+    res.status(ResponseConstants.HTTP_OK).send({})
+  }
+
+  async store (req: StoreBodyProps, res: Response) {
+    const fails = this._validator.withData(req.body).withRules([
+      {
+        fieldName: 'name',
+        rules: ['required', 'min:3', 'max:200']
+      },
+      {
+        fieldName: 'email',
+        rules: ['required', 'min:5', 'max:200']
+      },
+      {
+        fieldName: 'password',
+        rules: ['required', 'min:8', 'max:200']
+      }
+    ]).fails()
+
+    if (fails) {
+      return res.status(ResponseConstants.HTTP_BAD_REQUEST).send({
+        message: 'Invalid data',
+        errors: this._validator.errors()
+      })
+    }
+
     const { name, email, password } = req.body
-    const user = await repo.store({
+
+    const accountWithTheSameEmail = await this._repo.count({ email })
+
+    if (accountWithTheSameEmail) {
+      return res.status(ResponseConstants.HTTP_UNPROCESSABLE_ENTITY).send({
+        message: 'E-mail already taken'
+      })
+    }
+
+    const user = await this._repo.store({
       name,
       email,
       password
     })
 
-    res.status(response.HTTP_CREATED).json(user)
+    res.status(ResponseConstants.HTTP_CREATED).json(user)
   }
 
-  public update (req: Request, res: Response): Response {
-    return res.send()
+  async update (req: AuthenticatedRequest, res: Response) {
+    const fails = this._validator.withData(req.body).withRules([
+      {
+        fieldName: 'name',
+        rules: ['min:3', 'max:200']
+      },
+      {
+        fieldName: 'email',
+        rules: ['min:5', 'max:200']
+      },
+      {
+        fieldName: 'password',
+        rules: ['min:8', 'max:200']
+      }
+    ]).fails()
+
+    if (fails) {
+      return res.status(ResponseConstants.HTTP_BAD_REQUEST).send({
+        message: 'Invalid data',
+        errors: this._validator.errors()
+      })
+    }
+
+    const user = await this._repo.update(req.user.id, req.body)
+
+    if (!user) {
+      return res.status(ResponseConstants.HTTP_UNPROCESSABLE_ENTITY).send({
+        message: 'It was not possible to update your data'
+      })
+    }
+
+    res.status(ResponseConstants.HTTP_OK).send(user)
   }
 
-  public delete (req: Request, res: Response): Response {
+  delete (req: Request, res: Response): Response {
     return res.send()
   }
 }
 
-export default new UserController()
+export default UserController
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,8 +1,15 @@
 import { Router } from 'express'
-import userController from '../controllers/userController'
+import Hasher from '../security/hasher'
+import UserController from '../controllers/userController'
+import Validator from '../validators/validator'
+
+const userController = new UserController(new Validator(), new Hasher())
 
 const router = Router()
 
-router.post('/auth/register', userController.store)
+router.post('/auth/register', userController.store.bind(userController))
+router.post('/auth/login', userController.auth.bind(userController))
+router.get('/auth/data', userController.show.bind(userController))
+// router.put('/auth/data', userController.update.bind(userController))
 
 export default router
