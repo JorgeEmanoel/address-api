4ee20c6f2db791d079cdd5f090ea49be52f51cb5
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-04 00:41:22 -0300

    chore: add all and delete methods to iRepositor interface

--- src/contracts/iRepository.ts
+++ src/contracts/iRepository.ts
@@ -1,8 +1,10 @@
 interface IRepository<T, CP, SP, UP, FP, FR> {
   count: (attributes: CP) => Promise<number>
   find: (attributes: FP) => Promise<FR | null>
+  all: (attributes: FP) => Promise<T[] | null>
   store: (data: SP) => Promise<T>
   update: (id: number, data: UP) => Promise<T | null>
+  delete: (id: number) => Promise<T | null>
 }
 
 export default IRepository
