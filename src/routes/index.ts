c9355a16f6906a184f5b7fbebc676b813ebd40f7
Jorge Emanoel <eu.jorge.emanoell@gmail.com>
2022-02-04 00:43:18 -0300

    feat(address CRUD): address routes

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -1,17 +1,25 @@
 import { Router } from 'express'
 import Hasher from '../security/hasher'
 import UserController from '../controllers/userController'
+import AddressController from '../controllers/addressController'
 import Validator from '../validators/validator'
 import authMiddleware from '../middlewares/authMiddleware'
 
 const userController = new UserController(new Validator(), new Hasher())
+const addressController = new AddressController(new Validator())
 
 const router = Router()
 
 router.post('/auth/register', userController.store.bind(userController))
 router.post('/auth/login', userController.auth.bind(userController))
-
 router.get('/auth/data', authMiddleware, userController.show.bind(userController))
 router.put('/auth/data', authMiddleware, userController.update.bind(userController))
+router.delete('/auth/data', authMiddleware, userController.delete.bind(userController))
+
+router.get('/addresses', authMiddleware, addressController.index.bind(addressController))
+router.post('/addresses', authMiddleware, addressController.store.bind(addressController))
+router.get('/addresses/:id', authMiddleware, addressController.show.bind(addressController))
+router.put('/addresses/:id', authMiddleware, addressController.update.bind(addressController))
+router.delete('/addresses/:id', authMiddleware, addressController.delete.bind(addressController))
 
 export default router
